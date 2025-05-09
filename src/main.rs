47de8e096a06286f6e3da0432c23b69b42bb3223
UnusualEgg <shotgun282006@gmail.com>
2025-05-09 20:54:13 +0000

    mutex->static mut

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,68 @@
+#![allow(static_mut_refs)]
+
 #[macro_use]
 extern crate lazy_static;
-use std::ffi::c_int;
-use parking_lot::{Mutex, MutexGuard};
+use std::mem::MaybeUninit;
 mod state;
 use raylib::prelude::*;
 pub(crate) use state::State;
 
-//use raylib::prelude::*;
 
 #[cfg(target_family = "wasm")]
-extern "C" {
-    fn emscripten_set_main_loop(loop_fn: extern "C" fn(), fps: c_int, sim_infinite_loop: c_int);
-    fn emscripten_pause_main_loop();
+mod emscripten {
+    use std::ffi::c_int;
+    extern "C" {
+        fn emscripten_set_main_loop(loop_fn: extern "C" fn(), fps: c_int, sim_infinite_loop: c_int);
+        fn emscripten_pause_main_loop();
+    }
+    pub fn set_main_loop(loop_fn: extern "C" fn(), fps:u32, sim_infinite_loop:bool) {
+        unsafe {
+            emscripten_set_main_loop(loop_fn,fps as c_int, sim_infinite_loop as c_int); 
+        }
+    }
+    pub fn pause_main_loop() {
+        unsafe {
+            emscripten_pause_main_loop();
+        }
+    }
 }
-lazy_static! {
-    static ref STATE: Mutex<State> = Mutex::new(State::new());
-}
+
+static mut STATE: MaybeUninit<State> = MaybeUninit::uninit();
+static mut SHOULD_EXIT: bool = false;
 extern "C" fn draw_loop() {
-    let mut state: MutexGuard<State> = STATE.lock();
+    let state: &mut State = unsafe {STATE.assume_init_mut()};
     state.update();
     state.draw();
 
     let rl: &RaylibHandle = state.rl.as_ref().unwrap();
-    if rl.is_key_pressed(KeyboardKey::KEY_RIGHT) {
+    if rl.is_key_pressed(KeyboardKey::KEY_ESCAPE) {
         println!("Goodbye!");
-        state.rl = None;
-        state.t = None;
-        #[cfg(target_family = "wasm")]
         unsafe {
-            emscripten_pause_main_loop();
-            return;
+            SHOULD_EXIT=true;
         }
-        #[cfg(not(target_family = "wasm"))]
-        std::process::exit(0);
+    }
+    if rl.window_should_close() {
+        unsafe {
+            SHOULD_EXIT=true;
+        }
     }
 }
 
 fn main() {
     println!("Hewroo world :3!");
-    #[cfg(target_family = "wasm")]
     unsafe {
-        emscripten_set_main_loop(draw_loop, 0, 0);
+        STATE.write(State::new());
     }
+    #[cfg(target_family = "wasm")]
+    emscripten::set_main_loop(draw_loop, 0, false);
 
     #[cfg(not(target_family = "wasm"))]
-    {
-        while !STATE.lock().rl.as_ref().unwrap().window_should_close() {
-            draw_loop();
-        }
-        let mut state = STATE.lock();
-        state.rl = None;
-        state.t = None;
+    while !unsafe{SHOULD_EXIT} {
+        draw_loop();
+    }
+    //drop STATE
+    unsafe {
+        STATE.assume_init_drop();
     }
     println!("end of main!");
 }
